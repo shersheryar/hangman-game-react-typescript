9574365ea5f107b98355d1535ec5104cb939aac2
Sheryar Sher <21p827@pwr.nu.edu.pk>
2025-05-14 02:37:03 +0500

    done with keyboard code

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,9 @@ function App() {
       </div>
       <HangmanDrawing/>
       <HangmanWord/>
-      {/* <Keyboard/> */}
+      <div style={{alignSelf: "stretch"}}>
+      <Keyboard/>
+      </div>
 
     </div>
   );
