eb3d4880a502b5f34609ad0c294f1bb3113de341
Sheryar Sher <21p827@pwr.nu.edu.pk>
2025-05-14 00:56:16 +0500

    done with initial login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
+import { useState } from "react";
+import words from "./wordList.json";
 
 function App() {
+  //
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   return (
-    <>
-     <h1> hello world</h1>
-    </>
-  )
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        alignItems: "center",
+        margin: "0 auto",
+      }}
+    >
+      <div
+        style={{
+          fontSize: "2rem",
+          textAlign: "center",
+        }}
+      >
+        Lose Win
+      </div>
+      <HandmanDrawing/>
+      <HandmanWord/>
+      <Keyboard/>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
