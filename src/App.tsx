369c19c189000c6e9d43b6d379f222ce203968c8
Sheryar Sher <21p827@pwr.nu.edu.pk>
2025-05-14 02:01:09 +0500

    guessed word code done

--- src/App.tsx
+++ src/App.tsx
@@ -33,8 +33,8 @@ function App() {
         Lose Win
       </div>
       <HangmanDrawing/>
-      {/* <HangmanWord/>
-      <Keyboard/> */}
+      <HangmanWord/>
+      {/* <Keyboard/> */}
 
     </div>
   );
